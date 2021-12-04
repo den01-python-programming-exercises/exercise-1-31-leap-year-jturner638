823c2f6c43b272c189f852448fc77f0d9b4e8952
Justin Turner <mrfathed@protonmail.com>
2021-12-04 12:07:25 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    year = int(input("Give a year:"))
+
+    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+        print("The year is a leap year.")
+    else:
+        print("The year is not a leap year.")
 
 if __name__ == '__main__':
     main()
